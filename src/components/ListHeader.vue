a03211b8013e4ee1c803fbb212d2e432b49e2b1b
Binary1000 <929371489@qq.com>
2018-08-31 09:20:36 +0800

    test

--- src/components/ListHeader.vue
+++ src/components/ListHeader.vue
@@ -23,6 +23,11 @@
         font-size: 18px;
     }
 
+    div.table-header {
+        background-color: rgb(183, 183, 183);
+        user-select: none;
+    }
+
     td.my-col-1{
         width: 10%;
     }
